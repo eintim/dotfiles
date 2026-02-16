d9e7000c48db9a7e919ff8b035d4fb92b3daec40
Tim Horlacher <tim.horlacher@protonmail.com>
2026-02-16 22:47:45 +0100

    add git

--- system/vm/configuration.nix
+++ system/vm/configuration.nix
@@ -55,6 +55,8 @@
     variant = "";
   };
 
+  services.vmwareGuest.enable = true;
+
   # Configure console keymap
   console.keyMap = "de";
 
@@ -101,6 +103,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
