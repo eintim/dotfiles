dfe3d923830416e2977c147f3beea7d178bb5390
Tim Horlacher <tim.horlacher@protonmail.com>
2026-02-16 23:34:01 +0100

    add dependency

--- users/eintim/home.nix
+++ users/eintim/home.nix
@@ -17,6 +17,17 @@
     claude-code
   ];
 
+  programs.alacritty.enable = true; # Super+T in the default setting (terminal)
+  programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
+  programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
+  programs.waybar.enable = true; # launch on startup in the default setting (bar)
+  services.mako.enable = true; # notification daemon
+  services.swayidle.enable = true; # idle management daemon
+  services.polkit-gnome.enable = true; # polkit
+  home.packages = with pkgs; [
+    swaybg # wallpaper
+  ];
+
   programs.zsh = {
     enable = true; 
     autosuggestion.enable = true;
